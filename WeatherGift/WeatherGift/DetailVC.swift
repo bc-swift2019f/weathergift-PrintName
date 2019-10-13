5a54f083f30525d36d8f489667b6fb2d9e2041be
Allen <54394182+PrintName@users.noreply.github.com>
2019-10-13 15:13:43 -0400

    7.2 Complete

---
+++ WeatherGift/WeatherGift/DetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Allen Li on 10/13/19.
+//  Copyright © 2019 Allen Li. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

9b71ab5f374908e8d3bed2922e58181f1a316afc
Allen <54394182+PrintName@users.noreply.github.com>
2019-10-19 18:21:49 -0400

    7.16 Complete

--- WeatherGift/WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherGift/WeatherLocation.swift
@@ -8,15 +8,31 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
-struct WeatherLocation {
+class WeatherLocation {
     var name = ""
     var coordinates = ""
+    var currentTemp = "--"
+    var summary = ""
     
-    func getWeather() {
+    func getWeather(completed: @escaping () -> ()) {
         let weatherURL = urlBase + urlAPIKey + coordinates
         AF.request(weatherURL).responseJSON { response in
-            print(response)
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                if let temperature = json["currently"]["temperature"].double {
+                    let roundedTemp = String(format: "%3.f", temperature)
+                    self.currentTemp = roundedTemp + "°"
+                    self.summary = json["daily"]["summary"].string!
+                } else {
+                    print("Could not return a temperature")
+                }
+            case .failure(let error):
+                print(error)
+            }
+            completed()
         }
     }
 }
